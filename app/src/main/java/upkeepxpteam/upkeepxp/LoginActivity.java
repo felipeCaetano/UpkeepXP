540b8d487a8e30a7b47a43f630f61ddf74b47185
felipeCaetano <felipecmelo@gmail.com>
2017-11-28 09:38:11 -0300

    Login ao servidor externo realizado com sucesso.

--- app/src/main/java/upkeepxpteam/upkeepxp/LoginActivity.java
+++ app/src/main/java/upkeepxpteam/upkeepxp/LoginActivity.java
@@ -1,12 +1,21 @@
 package upkeepxpteam.upkeepxp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import upkeepxpteam.serverlayer.Conexao;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,6 +23,10 @@ public class LoginActivity extends AppCompatActivity {
     private EditText senha;
     private TextView esqueceuSenha;
     private Button logar;
+
+    //necessarios para conexão com a classe Conexao
+    private String url = "";
+    private String parametros = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +38,7 @@ public class LoginActivity extends AppCompatActivity {
         logar = findViewById(R.id.btn_logar);
         esqueceuSenha = findViewById(R.id.txt_forgotpass);
 
+        //Para recuperar a senha
         esqueceuSenha.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
@@ -33,7 +47,66 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        //Para logar no sistema
+        logar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //snippet para verificar o status da conexão
+                ConnectivityManager cm =
+                        (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+                //aqui pode gerar exception??
+                NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+                boolean isConnected = activeNetwork != null &&
+                        activeNetwork.isConnectedOrConnecting();
+                if (isConnected){
 
+                    String emailUser = email.getText().toString();
+                    String senhaUser = senha.getText().toString();
 
+                    if(emailUser.isEmpty() || senhaUser.isEmpty()){
+                        Toast.makeText(LoginActivity.this, getString(R.string.campo_vazio), Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        url = "http://179.106.9.69:8090/upkeepxp/login/logar.php";
+                        parametros = "email=" + emailUser +"&senha=" + senhaUser;
+                        new SolicitaDados().execute(url);
+                    }
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, getString(R.string.connection_failed), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    /*
+    Usa asyncTasks!
+    A classe interna a seguir conecta a internet e envia informações em segundo plano
+     */
+    private class SolicitaDados extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... url) {
+
+            return Conexao.postDados(url[0], parametros);
+        }
+
+        //exibe os resultados
+        @Override
+        protected void onPostExecute(String results){
+            if(results.contains("login_ok")){
+                Intent autentication = new Intent(LoginActivity.this,TelaInicialActivity.class);
+                startActivity(autentication);
+            }
+            else {
+                Toast.makeText(LoginActivity.this, getString(R.string.userPass_failed), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
