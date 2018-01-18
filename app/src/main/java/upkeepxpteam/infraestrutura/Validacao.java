c16c79ce9ac7f2020d004141247a87a8226ff9b4
felipeCaetano <felipecmelo@gmail.com>
2018-01-18 00:00:38 -0200

    Update Validacao.java

--- app/src/main/java/upkeepxpteam/infraestrutura/Validacao.java
+++ app/src/main/java/upkeepxpteam/infraestrutura/Validacao.java
@@ -6,17 +6,70 @@ package upkeepxpteam.infraestrutura;
 
 public class Validacao {
 
-    public Boolean validarNome(String nome){return true;}
-    public Boolean validarEmail(String email){return true;}
-    public Boolean validarSobrenome(String sobreNome){return true;}
-    public Boolean validarNascimento(String Nascimento){return true;}
-    public Boolean validarSexo(String sexo){return true;}
-    public Boolean validarTelefone(String telefone){return true;}
-    public Boolean validarEspecialidade(String especialidade){return true;}
-    public Boolean validarNumero(String numero){return true;}
-    public Boolean validarUF(String uf){return true;}
-    public Boolean validarFuncao(String funcao){return true;}
-    public Boolean validarLocal(String local){return true;}
-    public Boolean validarDescricao(String descricao){return true;}
+    public Boolean validarNome(String nome){
+
+        return nome.matches("^(?![ ])(?!.*[ ]{2})((?:e|da|do|das|dos|de|d'|D'|la|las|el|los)" +
+                "\\s*?|(?:[A-Z][^\\s]*\\s*?)(?!.*[ ]$))+$");
+    }
+    public Boolean validarEmail(String email){
+
+        return email.matches("[A-Za-z0-9\\._-]+@[A-Za-z]+\\.[A-Za-z]");
+    }
+    public Boolean validarSobrenome(String sobreNome){
+
+        return (sobreNome.matches("^(?![ ])(?!.*[ ]{2})((?:e|da|do|das|dos|de|d'|D'|la|las|el|los)" +
+                "\\s*?|(?:[A-Z][^\\s]*\\s*?)(?!.*[ ]$))+$"));
+    }
+    public Boolean validarNascimento(String Nascimento){
+
+        return Nascimento.matches("^((0[1-9]|[12]\\d)\\/(0[1-9]|1[0-2])|30\\/(0[13-9]|1[0-2])|31\\/(0[13578]|1[02]))\\/\\d{4}$");
+    }
+    public Boolean validarSexo(String sexo){
+        boolean result;
+        if (sexo.equals("M") || sexo.equals("F")){
+            result = true;
+        } else result = false;
+        return result;
+    }
+    public Boolean validarTelefone(String telefone) {
+
+        return telefone.matches("^[1-9]{2}\\-[2-9][0-9]{7,8}$");
+    }
+    public Boolean validarEspecialidade(String especialidade){
+
+        return true;
+    }
+    public Boolean validarNumero(String numero){
+
+        return numero.matches("^[0-9]{0,5}+$");
+    }
+    public Boolean validarUF(String uf){
+
+        return uf.matches("^(AC|AL|AP|AM|BA|CE|DF|ES|GO|MA|MT|MS|MG|PA|PB|PR|PE|PI|RJ|RN|RS|RO|RR|SC|SP|SE|TO)+$");
+    }
+    public Boolean validarFuncao(String funcao){
+
+        return funcao.matches("[A-Za-z]");
+    }
+    public Boolean validarLocal(String local){
+        boolean result = true;
+        if (local.isEmpty()) {
+            result = false;
+        }
+        return result;
+    }
+    public Boolean validarDescricao(String descricao){
+        boolean result = true;
+        if (descricao.isEmpty()) {
+            result = false;
+        }
+        return result;}
+
+    public Boolean validarHora (String hora) {
+
+        return hora.matches("^(0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])$");
+        }
+
+}
 
 }
